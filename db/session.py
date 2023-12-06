3a3d4fd22759a7a81f522de19d6e85c98f850d23
IuxMorti <zdracte21@bk.ru>
2023-12-06 20:06:13 +0500

    раскомментировал код с redis_session

--- db/session.py
+++ db/session.py
@@ -5,20 +5,18 @@ from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from db.models import User
-from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, REDIS_HOST, REDIS_PORT, REDIS_PASS
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-#redis_session = redis.asyncio.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASS, decode_responses=True)
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-#
-# async def get_redis_async_session() -> AsyncGenerator[redis.Redis, None]:
-#     async with redis.asyncio.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASS,
-#                                          decode_responses=True) as session:
-#         yield session
+
+async def get_redis_async_session() -> AsyncGenerator[redis.Redis, None]:
+    async with redis.asyncio.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASS,
+                                         decode_responses=True) as session:
+        yield session
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
